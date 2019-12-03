c6a891dc2a0c590494cf7cee2339664f1d2dce77
Khôi Tran <khoi.tran@sbb.ch>
2019-12-03 23:57:23 +0100

    add cors

--- src/Main.ts
+++ src/Main.ts
@@ -1,3 +1,4 @@
+import cors from 'cors';
 import compression from 'compression';
 import * as bodyParser from 'body-parser';
 import { Server } from '@overnightjs/core';
@@ -13,6 +14,7 @@ class TopologyServer extends Server {
         this.app.use(bodyParser.json());
         this.app.use(bodyParser.urlencoded({ extended: true }));
         this.app.use(compression());
+        this.app.use(cors());
         const t = new TrackEdgeController(db);
         this.addControllers([t]);
     }
